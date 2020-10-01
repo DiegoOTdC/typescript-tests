ecd968bf6bf54221ea5828596d1be8a9d642a68c
DiegoOTdC <studioartdiego@gmail.com>
2020-10-01 23:17:31 +0200

    add string to variable, pass as param, add to console.log()

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-function add(num1: number, num2: number, showResult: boolean) {
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
   //   if (typeof num1 === "number" && typeof num2 === "number") {
   //     throw new Error("Incorrect input!");
   //   }
   if (showResult) {
-    console.log(num1 + num2);
+    console.log(phrase + num1 + num2);
   } else {
     return num1 + num2;
   }
@@ -12,5 +12,6 @@ function add(num1: number, num2: number, showResult: boolean) {
 const number1 = 15;
 const number2 = 2.3;
 const printResult = true;
+const resultPhrase = "Result is: ";
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
