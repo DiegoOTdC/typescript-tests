3ae6d664a11c794792c225b5c7cf16610c06afa1
DiegoOTdC <studioartdiego@gmail.com>
2020-10-01 23:56:22 +0200

    add an array of strings type. For loop logging hobbies to upper case

--- app.ts
+++ app.ts
@@ -5,5 +5,14 @@
 const person = {
   name: "Diego",
   age: 28,
+  hobbies: ["Programming", "Problem Solving"],
 };
+
+let favoriteActivities: string[]; //an array of strings
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
