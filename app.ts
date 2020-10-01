71d8b0bb9cceece8d7df9c45510b691719a1d0df
DiegoOTdC <studioartdiego@gmail.com>
2020-10-01 22:40:23 +0200

    setup basic function adding numbers

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("your code goes here...!!");
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const number1 = 15;
+const number2 = 2.3;
+
+const result = add(number1, number2);
+console.log(result);
