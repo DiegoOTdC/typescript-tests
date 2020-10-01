505d043af86b4c764e60a1909bb3ca80f1d18b9e
DiegoOTdC <studioartdiego@gmail.com>
2020-10-01 23:33:40 +0200

    change app.ts to basics.ts, create new app.ts with object

--- app.ts
+++ app.ts
@@ -1,19 +1,5 @@
-function add(num1: number, num2: number, showResult: boolean, phrase: string) {
-  //   if (typeof num1 === "number" && typeof num2 === "number") {
-  //     throw new Error("Incorrect input!");
-  //   }
-
-  const result = num1 + num2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
-
-const number1 = 15;
-const number2 = 2.3;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1, number2, printResult, resultPhrase);
+const person = {
+  name: "Diego",
+  age: 28,
+};
+console.log(person);
---
+++ basics.ts
@@ -0,0 +1,19 @@
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+  //   if (typeof num1 === "number" && typeof num2 === "number") {
+  //     throw new Error("Incorrect input!");
+  //   }
+
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 15;
+const number2 = 2.3;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
