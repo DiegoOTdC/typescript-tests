c7b38ff291cb1ce8e9d35325c1e1eb7b7be98c4d
DiegoOTdC <studioartdiego@gmail.com>
2020-10-02 00:05:42 +0200

    add example of what doesn't work with Tuple

--- app.ts
+++ app.ts
@@ -11,8 +11,10 @@ const person: {
   role: [2, "author"], //array of 2 items, first always number, second always string. -> union type
 };
 
-person.role.push("admin"); // we can push this , TS doesn't know we just want 2 elements.
+// person.role.push("admin"); // we can push this , TS doesn't know we just want 2 elements. ->Push is an exception!
 // person.role[1] = 10; // TS also doesn't know we want the 2nd one to be a string.
+
+//person.role = [0, "admin", "user"] // this is not allowed with Tuple.
 
 let favoriteActivities: string[]; //an array of strings
 
