5bba603cad300fba1a2a70f5c41e26bfee65be34
DiegoOTdC <studioartdiego@gmail.com>
2020-10-02 11:09:35 +0200

    add enum/ custom type

--- app.ts
+++ app.ts
@@ -1,14 +1,35 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //this is a Tuple!
-} = {
-  // const person = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //this is a Tuple!
+// } = {
+//   // const person = {
+//   name: "Diego",
+//   age: 28,
+//   hobbies: ["Programming", "Problem Solving"],
+//   role: [2, "author"], //array of 2 items, first always number, second always string. -> union type
+// };
+
+// enum type // custom type
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+//You can give each property it's own number or even string in the enum.
+// enum Role {
+//     ADMIN = 20,
+//     READ_ONLY = "READ_ONLY",
+//     AUTHOR = 300,
+// }
+
+const person = {
   name: "Diego",
   age: 28,
   hobbies: ["Programming", "Problem Solving"],
-  role: [2, "author"], //array of 2 items, first always number, second always string. -> union type
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin"); // we can push this , TS doesn't know we just want 2 elements. ->Push is an exception!
@@ -24,4 +45,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
