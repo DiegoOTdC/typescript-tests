676ab8b8f0cc29a9a117b46475aecc45cf683a09
DiegoOTdC <studioartdiego@gmail.com>
2020-10-06 13:14:39 +0200

    literal type, conversion before combining values

--- app.ts
+++ app.ts
@@ -7,16 +7,20 @@ function combine(
 ) {
   let result;
   // run time type check -> depending on the logic, you do not always need to do this.
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if (resultConversion === "as-number") {
-    return +result; // parseFloat(result)
-  } else {
-    return result.toString();
-  }
+  return result;
+  //   if (resultConversion === "as-number") {
+  //     return +result; // parseFloat(result)
+  //   } else {
+  //     return result.toString();
+  //   }
 }
 
 const combinedAges = combine(30, 26, "as-number");
