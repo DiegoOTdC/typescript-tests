f9d19f0ab35b314912f4437fbc2aa79295f40a69
DiegoOTdC <studioartdiego@gmail.com>
2020-10-06 12:50:15 +0200

    add union type

--- app.ts
+++ app.ts
@@ -1,55 +1,18 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; //this is a Tuple!
-// } = {
-//   // const person = {
-//   name: "Diego",
-//   age: 28,
-//   hobbies: ["Programming", "Problem Solving"],
-//   role: [2, "author"], //array of 2 items, first always number, second always string. -> union type
-// };
+//Union type / output numbers and strings (or any kind of types really)
 
-// enum type // custom type
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  // run time type check -> depending on the logic, you do not always need to do this.
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-//You can give each property it's own number or even string in the enum.
-// enum Role {
-//     ADMIN = 20,
-//     READ_ONLY = "READ_ONLY",
-//     AUTHOR = 300,
-// }
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-const person = {
-  name: "Diego",
-  age: 28,
-  hobbies: ["Programming", "Problem Solving"],
-  role: Role.ADMIN,
-};
-
-// person.role.push("admin"); // we can push this , TS doesn't know we just want 2 elements. ->Push is an exception!
-// person.role[1] = 10; // TS also doesn't know we want the 2nd one to be a string.
-
-//person.role = [0, "admin", "user"] // this is not allowed with Tuple.
-
-// let favoriteActivities: string[]; //an array of strings
-// favoriteActivities = ["Sports"];
-
-//Any type // works, but try to avoid. You want to assign types to values.
-let favoriteActivities: any[]; //an array of strings
-favoriteActivities = ["Sports"];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.ADMIN) {
-  console.log("is admin");
-}
+const combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
