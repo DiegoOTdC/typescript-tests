9f230e2bb936e72c6ffefc4057d2da8e7339db29
DiegoOTdC <studioartdiego@gmail.com>
2020-10-08 22:04:34 +0200

    spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("your code goes here...!!");
+//spread operator
+const hobbies = ["sports", "cooking"];
+const activeHobbies = ["Hiking", ...hobbies];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: "Diego",
+  age: 28,
+};
+
+const copiedPerson = { ...person };
