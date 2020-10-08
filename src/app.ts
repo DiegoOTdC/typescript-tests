c0178ba95de1925e96e52628e8c39bbe18f18529
DiegoOTdC <studioartdiego@gmail.com>
2020-10-08 22:42:38 +0200

    create class in TS, with constructor method

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("your code goes here...!!");
+//classes start with upper case
+class Department {
+  name: string;
+
+  //method is called on creation, takes a string in this case that we assign to name property.
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+//create the Department object, passing it a string. store in const
+const accounting = new Department("Diego");
+
+console.log(accounting);
