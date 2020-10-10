e306f0cc15eac7f15cd10a68131a5bc932c66c56
DiegoOTdC <studioartdiego@gmail.com>
2020-10-10 14:51:50 +0200

    constructor function and this keyword

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,21 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  //method + this keyword, adding type 'Department' to 'this', creates extra safety.
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
 //create the Department object, passing it a string. store in const
 const accounting = new Department("Diego");
 
 console.log(accounting);
+
+accounting.describe();
+
+//'this' refers to the thing that is responsible for calling the method. accountingCopy in this case, which has no name property. Thus result, Department: undefined.
+//Add a name property, to have success here.
+const accountingCopy = { name: "Orlando", describe: accounting.describe };
+accountingCopy.describe();
