2f555d6157eadfaca095b904def17e8c1ed9580b
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 16:33:55 +0200

    intersection Types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("your code goes here...!!");
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+//if you want to use interfaces instead of types:
+//interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Diégo",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
