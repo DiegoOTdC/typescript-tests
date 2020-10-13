b72ae73f42e0537ab68bf281b4fb131fb6fd5236
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 19:15:28 +0200

    create generic function

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-//built-in generics -> allow you to get additional type information.
-//Array : Holds data with certain types
-const names: Array<string> = []; //string[]
-names[0].split(" "); //if no additional type info was added, you could have numbers in the array, which you can't use this method on.
+// //built-in generics -> allow you to get additional type information.
+// //Array : Holds data with certain types
+// const names: Array<string> = []; //string[]
+// names[0].split(" "); //if no additional type info was added, you could have numbers in the array, which you can't use this method on.
 
-//Promise : returns data with certain type
-//this example doesn't give an error, since you use any. In run time you will receive an error, since you can't use that split() method on a number.
-const promise: Promise<any> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// //Promise : returns data with certain type
+// //this example doesn't give an error, since you use any. In run time you will receive an error, since you can't use that split() method on a number.
+// const promise: Promise<any> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Diego", hobbies: ["Sports"] }, { age: 28 });
+console.log(mergedObj.age);
