b4f1be39c58f9683ce550439eab27f1e6fa6cee9
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 20:52:17 +0200

    keyof constraint

--- src/app.ts
+++ src/app.ts
@@ -65,3 +65,13 @@ console.log(countAndDescribe(["Sports", "Cooking"])); //array
 
 //Example that won't work
 // console.log(countAndDescribe(20)); //number...
+
+//keyof constraint
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+extractAndConvert({ name: "Diego" }, "name");
