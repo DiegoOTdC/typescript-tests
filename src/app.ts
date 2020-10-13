a9da0e32c89670701e7c568c937d493a6fa94aa4
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 12:54:42 +0200

    interfaces with classes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,39 @@
 //Create interface, always an object, set the types of properties and methods.
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-//create a variable, set type to (interface) Person.
-let user1: Person;
+//This class should implement the interface
+class Person implements Greetable {
+  name: string;
+  age = 28;
 
-//assign object to variable, where you assign strings and numbers etc to the properties and methods inside of the object.
-user1 = {
-  name: "Diego",
-  age: 28,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+//create a variable, set type to (interface) Person.
+let user1: Greetable;
+
+//assign object to variable, where you assign strings and numbers etc to the properties and methods inside of the object.
+// user1 = {
+//   name: "Diego",
+//   age: 28,
+//   greet(phrase: string) {
+//     console.log(phrase + " " + this.name);
+//   },
+// };
+
+// create new Person, feed in the name
+user1 = new Person("Diego");
 
 //call on the method.
 user1.greet("Hello! My name is");
+console.log(user1);
