f359a5332ae54ccdd5c1e2b8af1a1cb5db7372d6
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 19:27:29 +0200

    adding constraints

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,29 @@
 //   data.split(" ");
 // });
 
+//Creating Generic Function
+//function merge(objA: Object, objB: Object) => is too vague, you can't do the console.log at the end, because mergedObj wouldn't know if it had age or name or wichever property.
+//tell TS that you know objA and objB will most likely be 2 different kind of objects and to then return the intersection of the data.
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
+
+// const mergedObj = merge({ name: "Diego", hobbies: ["Sports"] }, { age: 28 });
+// console.log(mergedObj.age);
+
+//Constraints
+//This won't give an error, but is wrong nontheless, since Object.assign requires 2 objects, and we give an object and a number.
 function merge<T, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: "Diego", hobbies: ["Sports"] }, { age: 28 });
-console.log(mergedObj.age);
+const mergedObj = merge({ name: "Diego", hobbies: ["Sports"] }, 28);
+console.log(mergedObj);
+
+//Add constraints like this:
+function merge2<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj2 = merge2({ name: "Diego", hobbies: ["Sports"] }, { age: 28 });
+console.log(mergedObj2);
