9dd5d68cc54003164d8149c33252a1301660e906
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 17:07:59 +0200

    type guards

--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,68 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  //type guard using typeof
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  //type guard using ' in '
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+//possible output
+printEmployeeInfo(e1);
+printEmployeeInfo({ name: "Orlando", privileges: ["write-emails"] });
+printEmployeeInfo({ name: "George", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //can use in again
+  //   if ("loadCargo" in vehicle) {
+  //     vehicle.loadCargo(1000);
+  //   }
+
+  //or useinstanceof = operator in JS. JS is able to figure out if "vehicle" was based on "Truck" constructor function. (classes are translated to constructor functions)
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
