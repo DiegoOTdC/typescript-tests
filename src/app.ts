211b0c5a34a248476a89a25c2670393953dac944
DiegoOTdC <studioartdiego@gmail.com>
2020-10-13 18:06:49 +0200

    function overload

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,11 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+//function overload -> write all possibilities with the correct return type.
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
 function add(a: Combinable, b: Combinable) {
   //type guard using typeof
   if (typeof a === "string" || typeof b === "string") {
@@ -32,119 +37,123 @@ function add(a: Combinable, b: Combinable) {
   return a + b;
 }
 
-type UnknownEmployee = Employee | Admin;
+//without the function overload, result would be of type : Combinable, so you would not be able to use string methods like .split()
+const result = add("Diego", "Costa");
+result.split(" ");
 
-function printEmployeeInfo(emp: UnknownEmployee) {
-  console.log("Name: " + emp.name);
+// type UnknownEmployee = Employee | Admin;
 
-  //type guard using ' in '
-  if ("privileges" in emp) {
-    console.log("Privileges: " + emp.privileges);
-  }
+// function printEmployeeInfo(emp: UnknownEmployee) {
+//   console.log("Name: " + emp.name);
 
-  if ("startDate" in emp) {
-    console.log("Start Date: " + emp.startDate);
-  }
-}
+//   //type guard using ' in '
+//   if ("privileges" in emp) {
+//     console.log("Privileges: " + emp.privileges);
+//   }
 
-//possible output
-printEmployeeInfo(e1);
-printEmployeeInfo({ name: "Orlando", privileges: ["write-emails"] });
-printEmployeeInfo({ name: "George", startDate: new Date() });
+//   if ("startDate" in emp) {
+//     console.log("Start Date: " + emp.startDate);
+//   }
+// }
 
-class Car {
-  drive() {
-    console.log("Driving...");
-  }
-}
+// //possible output
+// printEmployeeInfo(e1);
+// printEmployeeInfo({ name: "Orlando", privileges: ["write-emails"] });
+// printEmployeeInfo({ name: "George", startDate: new Date() });
 
-class Truck {
-  drive() {
-    console.log("Driving a truck...");
-  }
+// class Car {
+//   drive() {
+//     console.log("Driving...");
+//   }
+// }
 
-  loadCargo(amount: number) {
-    console.log("Loading cargo ..." + amount);
-  }
-}
+// class Truck {
+//   drive() {
+//     console.log("Driving a truck...");
+//   }
 
-type Vehicle = Car | Truck;
+//   loadCargo(amount: number) {
+//     console.log("Loading cargo ..." + amount);
+//   }
+// }
 
-const v1 = new Car();
-const v2 = new Truck();
+// type Vehicle = Car | Truck;
 
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  //can use in again
-  //   if ("loadCargo" in vehicle) {
-  //     vehicle.loadCargo(1000);
-  //   }
+// const v1 = new Car();
+// const v2 = new Truck();
 
-  //or useinstanceof = operator in JS. JS is able to figure out if "vehicle" was based on "Truck" constructor function. (classes are translated to constructor functions)
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000);
-  }
-}
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   //can use in again
+//   //   if ("loadCargo" in vehicle) {
+//   //     vehicle.loadCargo(1000);
+//   //   }
 
-useVehicle(v1);
-useVehicle(v2);
+//   //or useinstanceof = operator in JS. JS is able to figure out if "vehicle" was based on "Truck" constructor function. (classes are translated to constructor functions)
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000);
+//   }
+// }
 
-interface Bird {
-  type: "bird"; //literal type
-  flyingSpeed: number;
-}
+// useVehicle(v1);
+// useVehicle(v2);
 
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
+// interface Bird {
+//   type: "bird"; //literal type
+//   flyingSpeed: number;
+// }
 
-type Animal = Bird | Horse;
+// interface Horse {
+//   type: "horse";
+//   runningSpeed: number;
+// }
 
-function moveAnimal(animal: Animal) {
-  let type;
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      type = animal.type;
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      type = animal.type;
-      speed = animal.runningSpeed;
-  }
-  console.log(`${type} moving at speed: ${speed}`);
-}
+// type Animal = Bird | Horse;
 
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-moveAnimal({ type: "horse", runningSpeed: 60 });
+// function moveAnimal(animal: Animal) {
+//   let type;
+//   let speed;
+//   switch (animal.type) {
+//     case "bird":
+//       type = animal.type;
+//       speed = animal.flyingSpeed;
+//       break;
+//     case "horse":
+//       type = animal.type;
+//       speed = animal.runningSpeed;
+//   }
+//   console.log(`${type} moving at speed: ${speed}`);
+// }
 
-//type casting: 1st way.
-// const userInputElement = <HTMLInputElement>(
-//   document.getElementById("user-input")!
-// );
+// moveAnimal({ type: "bird", flyingSpeed: 10 });
+// moveAnimal({ type: "horse", runningSpeed: 60 });
 
-//type casting: 2nd way. (works with React, because of JSX syntax)
-// const userInputElement = document.getElementById(
-//   "user-input"
-// )! as HTMLInputElement;
+// //type casting: 1st way.
+// // const userInputElement = <HTMLInputElement>(
+// //   document.getElementById("user-input")!
+// // );
 
-//If you are not sure this element won't return null, use an if statement instead of ! at the end.
-const userInputElement = document.getElementById("user-input");
+// //type casting: 2nd way. (works with React, because of JSX syntax)
+// // const userInputElement = document.getElementById(
+// //   "user-input"
+// // )! as HTMLInputElement;
 
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = "Hi there!";
-}
+// //If you are not sure this element won't return null, use an if statement instead of ! at the end.
+// const userInputElement = document.getElementById("user-input");
 
-//index properties: write flexible code. we don't need to know the amount of properties, or what we will name them, just the types.
-interface ErrorContainer {
-  //You can add a pre-defined property of the same type as the prop below:
-  // id: string;
-  [prop: string]: string;
-}
+// if (userInputElement) {
+//   (userInputElement as HTMLInputElement).value = "Hi there!";
+// }
 
-//now we can add an object containing all the error messages for example.
-const errorBag: ErrorContainer = {
-  email: "Not a valid email!",
-  userName: "Must start with a capital character!",
-};
+// //index properties: write flexible code. we don't need to know the amount of properties, or what we will name them, just the types.
+// interface ErrorContainer {
+//   //You can add a pre-defined property of the same type as the prop below:
+//   // id: string;
+//   [prop: string]: string;
+// }
+
+// //now we can add an object containing all the error messages for example.
+// const errorBag: ErrorContainer = {
+//   email: "Not a valid email!",
+//   userName: "Must start with a capital character!",
+// };
